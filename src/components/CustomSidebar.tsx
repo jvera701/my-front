f116a22f0fa3aca8379f265e016f8ff249f7f2be
Jose Alejandro <javera701@gmail.com>
2021-11-14 16:36:53 -0500

    merged custom sidebar with left sidebar and added ability to filter

--- src/components/CustomSidebar.tsx
+++ src/components/CustomSidebar.tsx
@@ -2,11 +2,14 @@ import React, { useEffect, useState } from 'react'
 import ThreadSidebar from './ThreadSidebar'
 import Form from 'react-bootstrap/Form'
 import '../assets/styles/Course.css'
-import { InputGroup } from 'react-bootstrap'
+import '../assets/styles/LeftSidebar.css'
+import { InputGroup, Button } from 'react-bootstrap'
+import { PencilSquare } from 'react-bootstrap-icons'
 import { Search } from 'react-bootstrap-icons'
 import customAxios from '../axios'
 import { useSelector } from 'react-redux'
 import { msToTime } from './Posts'
+import { useHistory } from 'react-router-dom'
 
 interface IName {
   name: string
@@ -29,6 +32,7 @@ function CustomSidebar() {
   const [courses, setCourses] = useState<TCourseSidebar>([])
   const [search, setSearch] = useState('')
   const course = useSelector((state: any) => state.course)
+  const history = useHistory()
 
   async function setData() {
     const resp: any = await customAxios({
@@ -51,6 +55,18 @@ function CustomSidebar() {
     setCourses(answer.data)
   }
 
+  async function requestFilter(filter) {
+    const answer: any = await customAxios({
+      url: customAxios.defaults.baseURL + '/thread/filter',
+      method: 'post',
+      data: {
+        courseId: course,
+        toFilter: filter,
+      },
+    })
+    setCourses(answer.data)
+  }
+
   function updateSearch(e) {
     e.preventDefault()
     setSearch(e.target.value)
@@ -68,33 +84,74 @@ function CustomSidebar() {
   }, [])
 
   return (
-    <div className='sidebar' onKeyUp={handleKeypress}>
-      <InputGroup>
-        <InputGroup.Text>
-          <Search />
-        </InputGroup.Text>
-        <Form.Control
-          placeholder='Search'
-          onChange={e => updateSearch(e)}
-          onSubmit={e => handleKeypress(e)}
-        />
-      </InputGroup>
-      {courses.length > 0
-        ? courses.map(course => {
-            const obj2 = {
-              title: course.title,
-              score: course.score,
-              category: course.category,
-              time: msToTime(course.createdAt),
-              replies: course.replies,
-              name: course.userId.name,
-              pinned: false,
-              _id: course._id,
-            }
-            return <ThreadSidebar key={course._id} {...obj2} />
-          })
-        : ''}
-    </div>
+    <>
+      <div className='left-sidebar'>
+        <Button
+          variant='post'
+          size='sm'
+          onClick={() => history.push('/home/' + course + '/createThread')}
+        >
+          <PencilSquare /> New Thread
+        </Button>
+        <div className='title-left-sidebar'> Courses</div>
+        <div className='courses-left-sidebar'>
+          <div className='course-link-sidebar'>Math </div>
+          <div className='course-link-sidebar'>Comp </div>
+          <div className='course-link-sidebar'>Comp123 </div>
+        </div>
+        <div className='title-left-sidebar'>Categories</div>
+        <div>
+          <div
+            className='category-left-sidebar'
+            onClick={() => requestFilter('General')}
+          >
+            <div className='square general'> </div>
+            General
+          </div>
+          <div
+            className='category-left-sidebar'
+            onClick={() => requestFilter('Lectures')}
+          >
+            <div className='square lectures'> </div>
+            Lectures
+          </div>
+          <div
+            className='category-left-sidebar'
+            onClick={() => requestFilter('Quizzes')}
+          >
+            <div className='square quizzes'> </div>
+            Quizzes
+          </div>
+        </div>
+      </div>
+      <div className='sidebar' onKeyUp={handleKeypress}>
+        <InputGroup>
+          <InputGroup.Text>
+            <Search />
+          </InputGroup.Text>
+          <Form.Control
+            placeholder='Search'
+            onChange={e => updateSearch(e)}
+            onSubmit={e => handleKeypress(e)}
+          />
+        </InputGroup>
+        {courses.length > 0
+          ? courses.map(course => {
+              const obj2 = {
+                title: course.title,
+                score: course.score,
+                category: course.category,
+                time: msToTime(course.createdAt),
+                replies: course.replies,
+                name: course.userId.name,
+                pinned: false,
+                _id: course._id,
+              }
+              return <ThreadSidebar key={course._id} {...obj2} />
+            })
+          : ''}
+      </div>
+    </>
   )
 }
 
--- src/pages/Course.tsx
+++ src/pages/Course.tsx
@@ -2,7 +2,6 @@ import React from 'react'
 import CustomNavbar from '../components/CustomNavbar'
 import CustomSidebar from '../components/CustomSidebar'
 import Posts from '../components/Posts'
-import LeftSidebar from '../components/LeftSidebar'
 import '../assets/styles/CoursePage.css'
 
 export default function Course() {
@@ -10,7 +9,6 @@ export default function Course() {
     <>
       <CustomNavbar />
       <div className='course-page'>
-        <LeftSidebar />
         <CustomSidebar />
         <Posts />
       </div>
